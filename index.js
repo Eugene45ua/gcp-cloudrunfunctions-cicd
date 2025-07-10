3e8c4e7d84fa5d66811967f29bdb0233c1af4e23
Eugene45ua <121547430+Eugene45ua@users.noreply.github.com>
2025-07-10 18:43:34 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+// NodeJS App for GCP Cloud Run Functions deployed via GCP Cloud Build Triggers
+// by Eugene
+// 2025
+
+
+exports.helloWorld = (req, res) => {
+  const message="<font color='blue'>СloudRunFunction from GCP by Eugene!</font><br><b>App Version 1.1</b>";
+  res.status(200).send(message);
+};
