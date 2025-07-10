bfb35fc5055ab0a726c9cc3582a7515086c93c17
Eugene45ua <121547430+Eugene45ua@users.noreply.github.com>
2025-07-10 21:46:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 
 
 exports.helloWorld = (req, res) => {
-  const message="<font color='blue'>СloudRunFunction from GCP by Eugene!</font><br><b>App Version 1.01</b>";
+  const message="<font color='blue'>СloudRunFunction from GCP by Eugene!</font><br><b>App Version 1.02</b>";
   res.status(200).send(message);
 };
